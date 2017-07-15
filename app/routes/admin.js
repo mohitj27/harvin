c02beb896a075e8e1c15b25f4c27699950532f8b
Gopal sharma <gopal151295@gmail.com>
2017-07-15 09:46:06 +0530

    File upload -2
    only files object is being saved to db

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,10 +2,21 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/User.js");
+var File = require("../models/File.js");
+var path = require('path');
+var multer = require('multer') ;
+var moment = require("moment-timezone");
+var fs = require("file-system");
 
-
-
-
+//setting disk storage for uploaded files
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './uploads')
+	},
+	filename: function(req, file, callback) {
+		callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+	}
+});
 
 //Handle file upload
 router.post('/uploadFile', function(req, res) {
@@ -13,8 +24,37 @@ router.post('/uploadFile', function(req, res) {
 		storage: storage
 	}).single('userFile')
 	upload(req, res, function(err) {
-        //TODO: save the file URI in mongodb
-		res.end('File is uploaded')
+        var currentTime = moment(Date.now()).tz("Asia/Kolkata").format('MMMM Do YYYY, h:mm:ss a');
+        var newFile = {
+            fileName:req.file.originalname,
+            fileType:path.extname(req.file.originalname),
+            filePath:req.file.path,
+            uploadDate:currentTime,
+            fileSize:req.file.size,
+            subjectName:req.body.subjectName,
+            chapterName:req.body.chapterName,
+            topicName:req.body.topicName
+        }
+
+        File.create(newFile, function(err, createdFile){
+            if(err){
+                console.log(err);
+
+                //deleting uploaded file from upload directory
+                fs.unlink(req.file.path, function(err){
+                    if(err) console.log("file deletion err\n" + err);
+                    else console.log("file deleted from uploads directory")
+                });
+
+                req.flash("error", err);
+                res.redirect("/admin/uploadFile");
+
+            }
+            else{
+                req.flash("success", req.file.originalname +" uploaded successfully");
+                res.redirect("/admin/uploadFile");
+            }
+        });
 	})
 });
 
