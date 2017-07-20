93a72e16b82169d1c01f6e9240fb6fb54898ae98
Gopal sharma <gopal151295@gmail.com>
2017-07-20 20:51:54 +0530

    add new in select list

--- app/public/script/main.js
+++ app/public/script/main.js
@@ -1,46 +1,44 @@
 $(function() {
   var content = "<input type=text onKeyDown='event.stopPropagation();' onKeyPress='addSelectInpKeyPress(this,event)' onClick='event.stopPropagation()' placeholder='Add item'> <span class='glyphicon glyphicon-plus addnewicon' onClick='addSelectItem(this,event,1);'></span>";
- 
+
   var divider = $('<option/>')
           .addClass('divider')
           .data('divider', true);
           
- 
+
   var addoption = $('<option/>')
           .addClass('additem')
           .data('content', content)
-      
-  $('#add').hide()
-          
-          .append(addoption)
+		  
+  $('.selectpicker')
           .append(divider)
+          .append(addoption)
           .selectpicker();
- 
+
 });
- 
+
 function addSelectItem(t,ev)
 {
+
    ev.stopPropagation();
+
    var txt=$(t).prev().val().replace(/[|]/g,"");
    if ($.trim(txt)=='') return;
-    var select = document.getElementById("select1");
-    var opt = document.createElement("option");
-    opt.value = txt;
-    opt.text=txt;
-    opt.selected= true;
+   var p=$(t).closest(".dropdown-menu.open").next();
 
-    select.appendChild(opt);
-    $(".selectpicker").selectpicker('refresh');
+   var o=$('option',p).eq(-2);
 
+   o.before( $("<option>", { "selected": true, "text": txt}) );
+   p.selectpicker('refresh');
 }
- 
+
 function addSelectInpKeyPress(t,ev)
 {
    ev.stopPropagation();
- 
+
    // do not allow pipe character
    if (ev.which==124) ev.preventDefault();
- 
+
    // enter character adds the option
    if (ev.which==13)
    {
