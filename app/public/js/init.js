266def205d053b1a2f5245ea6d78b652e1b2323b
ishank <ishank.web.dev@gmail.com>
2017-12-13 14:35:59 +0530

    landing page imporved

--- app/public/js/init.js
+++ app/public/js/init.js
@@ -1,21 +1,48 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function() {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     // $('.carousel').carousel();
-    $('.carousel.carousel-slider').carousel({fullWidth: true, indicator: true});
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicator: true
+    });
 
+    $('.tap-target').tapTarget('open');
+    state=true;
+    // $('.tap-target').tapTarget('close');
+
+
+    // Next slide
+
+    setInterval(function() {
+      $('.carousel').carousel('next');
+    }, 4000);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+var state=true;
+function tap() {
+  console.log('tap');
+  if (!state) {
+    $('.tap-target').tapTarget('open');
 
+    state = true;
+  } else {
+     $('.tap-target').tapTarget('close');
+     state=false;
 
-function scrollDown(){
-  
-$('html, body').animate({
-  scrollTop: $("#carousel1").offset().top
+  }
+
+}
+
+function scrollDown() {
+
+  $('html, body').animate({
+    scrollTop: $("#carousel1").offset().top
   }, 1000);
 
 }
+
 
 // $('.carousel.carousel-slider').carousel({fullWidth: true});
