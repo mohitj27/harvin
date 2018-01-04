219e249adad78b8e42afcb8bbe9159a7b5b39a5c
ishank <ishank.web.dev@gmail.com>
2018-01-04 11:53:21 +0530

    fixed login screen

--- app/public/js/initResults.js
+++ app/public/js/initResults.js
@@ -1,8 +1,12 @@
-$(document).ready(function(){
-   $('ul.tabs').tabs().addClass('green-tabs z-depth-2 no-pad-top')
-   // $('#navbar').removeClass('z-depth-2')
-   // $('#navbar').addClass('z-depth-0')
-   $('ul.tabs').onShow(curr=>{
-     console.log('tab selected')
-   })
- })
+$(document).ready(function() {
+  $('ul.tabs').tabs().addClass('green-tabs z-depth-2 no-pad-top')
+  // $('#navbar').removeClass('z-depth-2')
+  // $('#navbar').addClass('z-depth-0')
+  $('.chip').click(function() {
+    $('.chip').removeClass('active-chip-rev z-depth-4')
+    $(this).addClass('active-chip-rev z-depth-4')
+  })
+  $('ul.tabs').onShow(curr => {
+    console.log('tab selected')
+  })
+})
