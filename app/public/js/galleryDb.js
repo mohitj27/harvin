a06ebde8de5b6ae240faf7e7dd01eff99640e742
ishank <ishank.web.dev@gmail.com>
2018-01-16 13:35:54 +0530

    gallery completed

--- app/public/js/galleryDb.js
+++ app/public/js/galleryDb.js
@@ -1,10 +1,12 @@
 let allImages = []
-let slicedImages = {};
-let counter=0;
+let slicedImages = {}
+let currGallery = []
+let counter = 0;
 let perPage = 4;
 
 $('document').ready(function() {
-  getImages();
+  getImagesFromServer()
+
   $('.chip').click(function() {
     counter = 0;
     $('.chip').removeClass('z-depth-4 active-chip')
@@ -13,105 +15,122 @@ $('document').ready(function() {
       'color': ''
     })
     $(this).addClass('z-depth-4 active-chip')
-    let data = $(this).html().toLowerCase()
-    $('.gallery-image').fadeOut(1000)
+    let category = $(this).html().toLowerCase()
+    $('.gallery-image').fadeOut(500)
     $('.gallery-image').remove()
-    if(data === 'all'){
-      displayCurrentCategoryImages(allImages);
+    if (category == 'all')
+      currGallery = allImages
+    else
+      currGallery = slicedImages[category]
 
-    }else {
-      displayCurrentCategoryImages(slicedImages[data]);
+    displayCurrentGallery(currGallery)
+    $('.gallery-image').fadeIn(500)
 
-    }
   })
 
-  $('.pagination > li > a').on('click', (e) => {
-    console.log('click', $(e.currentTarget).text())
-    pageNumberToDisplay = $(e.currentTarget).text()
-    displayCurrentPage(pageNumberToDisplay)
+  $('.pagination').on('click','a.pagination-value', function() {
+    let pageNumberToDisplay = $(this).html()
+    $('.pagination-value').removeClass("active-chip")
+    $(this).addClass("active-chip")
+    console.log('page', pageNumberToDisplay)
+    displayCurrentPageWithCategory(pageNumberToDisplay - 1)
+
+
   })
-
-  $(window).scroll(function() {
-     if($(window).scrollTop() + $(window).height() == $(document).height()) {
-       moreImages()
-         console.log('hi');
-     }
-  });
-  // define options (if needed)
-  var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 1 // start at first slide
-  };
-
-  //
-  // let startGallery = function() {
-  //   // Initializes and opens PhotoSwipe
-  //   let gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options)
-  //
-  //   gallery.init()
-  //
-  // }
-  // $('.main-img').click(startGallery)
 
 
 });
 
-function moreImages(){
-console.log('moreImages')
-let data = $('.active-chip').html().toLowerCase()
-}
-
-function getImages(){
+function getImagesFromServer() {
   $.get("/db/gallery/all", function(res) {
-    console.log('res',res);
-    if(res){
+    if (res) {
       allImages = res.images;
+      currGallery = res.images;
       sliceIntoCategory(allImages)
-      displayCurrentCategoryImages(allImages);
+      displayCurrentGallery(allImages)
+
     }
   });
 }
 
-function displayCurrentCategoryImages(currentCategoryImages){
-  if(currentCategoryImages){
+function displayCurrentGallery(currentCategoryImages) {
+  if (currentCategoryImages) {
     currentCategoryImages.forEach(function(image, i) {
-      if(i < perPage){
-        counter = counter + 1;
-        console.log(counter)
-        let $imageElement = $("<div><img src=" + image.src + "></div>")
-        $imageElement.addClass("col s12 m4 l3 gallery-image")
-        $imageElement.children().addClass("responsive-img materialboxed")
-        // console.log($imageElement.children().height())
-          $('.gallery-container').append($imageElement)
-          $('.materialboxed').materialbox();
+      if (i < perPage) {
+        counter++;
+        makeGalleryElements(image)
       }
     })
+    displayPagination(currentCategoryImages.length)
+
   }
 }
 
-function sliceIntoCategory(allImages){
+function displayPagination(galleryLength) {
+  console.log(Math.ceil(galleryLength/perPage))
+  let i=0
+  let max=Math.ceil(galleryLength/perPage)
+  while ( i < max ) {
+    console.log(i)
+    let $pageElement = $("<li><a>"+(max-(i))+"</a> </li>")
+    $pageElement.children().attr("href","#!")
+    if((max-1-i)==0)
+    $pageElement.children().addClass("active-chip")
+    $pageElement.children().addClass("pagination-value")
+   $('.pagination li:nth-child(1)').after($pageElement)
+   i=i+1
+
+  }
+  $('.pagination').children().addClass("waves-effect")
+
+}
+
+function makeGalleryElements(image) {
+  let $imageElement = $("<div><img src=" + image.src + "></div>")
+  $imageElement.addClass("col s12 m4 l3 gallery-image")
+  $imageElement.children().addClass("responsive-img materialboxed")
+  $('.gallery-container').append($imageElement)
+  $('.materialboxed').materialbox();
+}
+
+function sliceIntoCategory(allImages) {
   slicedImages = {};
 
   allImages.forEach((image, i) => {
     var category = image.category;
-    if(slicedImages.hasOwnProperty(category)){
+    if (slicedImages.hasOwnProperty(category)) {
+      slicedImages[category].push(image)
+    } else {
+      slicedImages[category] = []
       slicedImages[category].push(image)
     }
-    else{
-    slicedImages[category]=[]
-    slicedImages[category].push(image)}
-
-  })
-
-  return slicedImages;
-}
-
-function displayCurrentPage(pageNumberToDisplay){
-  // console.log($('.gallery-container').children())
-
-  $imgNeedsToUpdate = $('.materialboxed');
-  $imgNeedsToUpdate.forEach((img, i) => {
-    img.attr('scr', currentCategoryImages[i + perPage * pageNumberToDisplay])
   })
 }
+/**function that shows images at the start and pagination for all (ALL CATEGORY)**/
+function displayCurrentPage(pageNumberToDisplay) {
+  $('.materialboxed').toArray().forEach((img, i) => $(img).attr('src', allImages[i + perPage * pageNumberToDisplay].thumbPath))
+}
+/**function that shows images at the start and pagination for all (ALL CATEGORY)**/
+function displayCurrentPageWithCategory(pageNumberToDisplay) {
+  console.log(currGallery)
+  $('.materialboxed').toArray().forEach((img, i) => {
+    console.log(i + perPage * pageNumberToDisplay, 'val')
+    let srcValue = currGallery[i + perPage * pageNumberToDisplay]
+    if (srcValue) {
+      $(img).show()
+      $(img).attr('src', srcValue.thumbPath)
+    } else {
+      console.log('hidden')
+      $(img).hide()
+
+    }
+  })
+}
+// $(".prev-button").click(function (){
+//   let pageNumberToDisplay = $("active-chip").html()
+//   $('.pagination-value').removeClass("active-chip")
+//   $(this).addClass("active-chip")
+//   console.log('page', pageNumberToDisplay)
+//   displayCurrentPageWithCategory(pageNumberToDisplay - 1)
+// })
+// $(".next-button").click(function (){})
