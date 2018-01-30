a49c1f42dc7cfef20c9d5407aaf24420ee92360b
ishank <ishank.web.dev@gmail.com>
2018-01-30 21:39:05 +0530

    fixed fron

--- app/public/js/blogInsert.js
+++ app/public/js/blogInsert.js
@@ -1,11 +1,19 @@
 
   $(function() {
-  
 
-    $('.button-collapse1').sideNav({
-      menuWidth: 250, // Default is 300
-      }
-  );
+
+
+
+      $('.button-collapse').sideNav({
+          menuWidth: 300, // Default is 300
+          edge: 'right', // Choose the horizontal origin
+          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          draggable: true, // Choose whether you can drag to open on touch screens,
+          onOpen: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
+          onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+        }
+      );
+
     var socket = io("http://localhost:3001/")
     socket.emit('message', 'Hello server');
 
