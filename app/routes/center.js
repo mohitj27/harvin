9d76d39b8c606f68b8e2f3134f218d1298389581
Gopal sharma <gopal151295@gmail.com>
2018-01-31 13:38:31 +0530

    partail changes assignment

--- app/routes/center.js
+++ app/routes/center.js
@@ -2,6 +2,7 @@ var express = require("express"),
   errors = require("../error"),
   middleware = require("../middleware"),
   Center = require('./../models/Center'),
+  Assignment = require('./../models/Assignment'),
   Exam = require('./../models/Exam'),
   router = express.Router();
 
@@ -30,15 +31,75 @@ router.get('/:centerName', middleware.isLoggedIn, middleware.isCentreOrAdmin, (r
 
 router.get('/:centerName/exams', middleware.isLoggedIn, middleware.isCentreOrAdmin, (req, res, next) => {
   let centerName = req.params.centerName;
-  Exam.find({
+  Center.findOne({
+    centerName
+  }, (err, foundCenter) => {
+    if (err) {
+      console.log('err', err);
+      next(new errors.generic())
+    } else if (!foundCenter) {
+      res.render('notFound');
+    } else {
+
+      Exam.find({
+        atCenter: foundCenter._id
+      }, (err, foundExams) => {
+        if (err) {
+          console.log('err', err);
+          return next(new errors.generic())
+        } else {
+          res.render('exams', {
+            foundExams
+          })
+        }
+      })
+
+    }
+  })
+
+})
+
+router.get('/:centerName/assignments', middleware.isLoggedIn, middleware.isCentreOrAdmin, (req, res, next) => {
+  let centerName = req.params.centerName;
+  Center.findOne({
+    centerName
+  }, (err, foundCenter) => {
+    if (err) {
+      console.log('err', err);
+      next(new errors.generic())
+    } else if (!foundCenter) {
+      res.render('notFound');
+    } else {
+
+      Assignment.find({
+        atCenter: foundCenter._id
+      }, (err, foundExams) => {
+        if (err) {
+          console.log('err', err);
+          return next(new errors.generic())
+        } else {
+          res.render('exams', {
+            foundExams
+          })
+        }
+      })
+
+    }
+  })
+
+})
+
+router.get('/:centerName/batches', middleware.isLoggedIn, middleware.isCentreOrAdmin, (req, res, next) => {
+  let centerName = req.params.centerName;
+  Assignment.find({
     atCenter: req.user._id
-  }, (err, foundExams) => {
+  }, (err, foundAssignments) => {
     if (err) {
       console.log('err', err);
       return next(new errors.generic())
     } else {
-      res.render('exams', {
-        foundExams
+      res.render('assignments', {
+        foundAssignments
       })
     }
   })
