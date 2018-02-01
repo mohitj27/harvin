eb4a915430e31cc7034905e658e8873032266a95
ishank <ishank.web.dev@gmail.com>
2018-02-01 20:53:11 +0530

    fixed nav in blog

--- app/public/js/blogInsert.js
+++ app/public/js/blogInsert.js
@@ -4,7 +4,7 @@ $(function() {
       $('ul.tabs').tabs();
 
 
-      $('.button-collapse').sideNav({
+      $('.button-collapse-image').sideNav({
         menuWidth: 300, // Default is 300
         edge: 'left', // Choose the horizontal origin
         draggable: true, // Choose whether you can drag to open on touch screens,
