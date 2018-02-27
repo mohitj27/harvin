f361a3c5102b3a9d83263e50b380a7a780cbd471
Gopal sharma <gopal151295@gmail.com>
2018-02-27 14:43:02 +0530

    all courses

--- app/routes/courses.js
+++ app/routes/courses.js
@@ -1,16 +1,13 @@
-const express = require('express'),
-  router = express.Router(),
-  FIOCont = require('../controllers/fileRW.controller'),
-  validator = require('validator'),
-  errorHandler = require('../errorHandler')
-coursesCont = require('../controllers/courses.controller'),
-  path = require('path')
-COURSEIMAGE_SAVE_LOCATION = path.normalize(__dirname + '/../../../HarvinDb/courseImages/');
-
+const express = require('express')
+const router = express.Router()
+const FIOCont = require('../controllers/fileRW.controller')
+const validator = require('validator')
+const errorHandler = require('../errorHandler')
+const coursesCont = require('../controllers/courses.controller')
+const path = require('path')
+const COURSEIMAGE_SAVE_LOCATION = path.normalize(__dirname + '/../../../HarvinDb/courseImages/')
 
 router.get('/new', (req, res) => {
-
-
   res.render('newCourse')
 })
 
@@ -23,29 +20,25 @@ router.get('/all', async (req, res) => {
   } catch (err) {
     next(err)
   }
-
 })
 
 router.get('/:courseId/edit', async (req, res, next) => {
+  res.locals.flashUrl = req.header.referer
 
-  res.locals.flashUrl = req.header.referer;
-
-  const courseId = req.params.courseId || '';
+  const courseId = req.params.courseId || ''
   if (!courseId || !validator.isMongoId(courseId)) return errorHandler.errorResponse('INVALID_FIELD', 'course id', next)
 
-    try {
-      const foundCourse = await coursesCont.findCourseById(courseId)
-      res.render('editCourse', {
-        foundCourse
-      })
-    } catch (err) {
-      next(err)
-    }
-
+  try {
+    const foundCourse = await coursesCont.findCourseById(courseId)
+    res.render('editCourse', {
+      foundCourse
+    })
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
-
   FIOCont.fileWriteMulterPromise(COURSEIMAGE_SAVE_LOCATION, 'courseImage').then((upload) => {
     upload(req, res, (err) => {
       if (err) next(err)
@@ -67,23 +60,18 @@ router.post('/', (req, res, next) => {
         courseFrequency
       }
 
-      if(req.file) {
+      if (req.file) {
         courseImage = req.file.originalname,
-        course.courseImage = courseImage
+          course.courseImage = courseImage
       }
       coursesCont.insertInCourse(course).then((result) => {
         if (result) {
-
-          req.flash("success", 'Course Inserted Successfully')
-          res.redirect("/admin/courses/all")
+          req.flash('success', 'Course Inserted Successfully')
+          res.redirect('/admin/courses/all')
         }
       }).catch(err => next(err))
-
     })
   }).catch(err => next(err))
-
-
-
 })
 router.delete('/delete/:courseName', (req, res, next) => {
   try {
