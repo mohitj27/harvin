1e17c47e6128111c83629ba176318f1054bf185d
ishank <ishank.web.dev@gmail.com>
2018-03-01 18:08:19 +0530

    blog admin interface complete

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -10,7 +10,8 @@ var express = require('express'),
   router = express.Router(),
   http = require('http').Server(app),
   io = require('socket.io')(http),
-  promise = require('bluebird')
+  promise = require('bluebird'),
+  blogCont=require('../controllers/blog.controller')
 
 io.on('connection', function (socket) {
   console.log('connected')
@@ -23,7 +24,7 @@ router.get('/new', middleware.isLoggedIn, middleware.isCentreOrAdmin, (req, res,
   res.render('newBlog')
 })
 
-router.get('/all', middleware.isLoggedIn, middleware.isCentreOrAdmin, (req, res, next) => {
+router.get('/all', (req, res, next) => {
   Blog.find({})
     .sort({
       'uploadDateUnix': -1
@@ -200,4 +201,12 @@ function checkBlogImageDir () {
     // console.log('not making blog dir')
   }
 }
+router.post('/editmode/:blogTitle/:mode/:check',async (req,res)=>{
+  let result= await blogCont.updateBlogMode(
+                                req.params.blogTitle,
+                                req.params.mode,
+                                req.params.check)
+res.send(result)
+
+})
 module.exports = router
