cfc7e59a6645878ac96a13907812ff021970bb8c
ishank <ishank.web.dev@gmail.com>
2018-03-15 17:19:23 +0530

    clipboard js

--- app/public/js/genlink.js
+++ app/public/js/genlink.js
@@ -1,4 +1,16 @@
 $(()=>{
 
+  var clipboard = new ClipboardJS('.clipboard');
+
+
+  clipboard.on('success', function(e) {
+      new Materialize.Toast('<span>Link Copied<span>',1000)
+  });
+
+  clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+  });
+
 
 })
