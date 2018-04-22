f24d9afb98439386c9ff9778d5edf47a8d7c9a3a
Gopal sharma <gopal151295@gmail.com>
2018-04-22 17:04:51 +0530

    added panel to add new ques + state

--- client/harvinreact/src/routes/app.jsx
+++ client/harvinreact/src/routes/app.jsx
@@ -4,49 +4,48 @@ import {
   ContentPaste,
   LibraryBooks,
   SdStorage,
+  NoteAdd
 } from 'material-ui-icons';
 import DashboardPage from '../views/Dashboard/Dashboard.jsx';
-
-
+import AddQues from '../views/AddQues/AddQues.jsx';
 
 const appRoutes = [
   {
     path: '/dashboard',
     sidebarName: 'Dashboard',
     icon: Dashboard,
-    component: DashboardPage,
-  },
-  {
+    component: DashboardPage
+  }, {
     path: '/makeoforder',
     sidebarName: 'Make Of Order',
     icon: Person,
-    component: DashboardPage,
-  },
-  {
+    component: DashboardPage
+  }, {
     path: '/makeofstock',
     sidebarName: 'Make Of Stock',
     icon: ContentPaste,
-    component: DashboardPage,
-  },
-  {
+    component: DashboardPage
+  }, {
     path: '/stats',
     sidebarName: 'Transactions',
     icon: LibraryBooks,
-    component: DashboardPage,
-  },
-
-  {
+    component: DashboardPage
+  }, {
     path: '/records',
     sidebarName: 'Records',
     icon: SdStorage,
-    component: DashboardPage,
-  },
-  {
+    component: DashboardPage
+  }, {
+    path: '/addQues',
+    sidebarName: 'Add Question',
+    icon: NoteAdd,
+    component: AddQues
+  }, {
     redirect: true,
     path: '/',
     to: '/dashboard',
-    navbarName: 'Redirect',
-  },
+    navbarName: 'Redirect'
+  }
 ];
 
 export default appRoutes;
