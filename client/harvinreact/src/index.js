f989be47277e4eafc14bafe1f08a76938347a7fa
ishank <ishank.web.dev@gmail.com>
2018-05-09 18:38:54 +0530

    public route added

--- client/harvinreact/src/index.js
+++ client/harvinreact/src/index.js
@@ -1,48 +1,49 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch,Redirect,Link } from "react-router-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import "./assets/css/material-dashboard-react.css";
-import rootReducer from "./reducers/index";
-import indexRoutes from "./routes/index.jsx";
-import logger from "redux-logger";
-import setAuthToken from './config/setAuthToken'
-import url from './config'
-import Login from './containers/Login/Login'
-const Public=Login
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import './assets/css/material-dashboard-react.css';
+import rootReducer from './reducers/index';
+import indexRoutes from './routes/index.jsx';
+import logger from 'redux-logger';
+import setAuthToken from './config/setAuthToken';
+import url from './config';
+import Login from './containers/Login/Login';
+import Public from './views/Public/Public';
 const hist = createBrowserHistory();
 const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    })
+  // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+})
     : compose;
 
 const enhancer = composeEnhancers(
   applyMiddleware(thunk, logger)
 );
-let token = localStorage.getItem('harvinStudentToken')
-console.log('token',token);
-if(url=='http://localhost:3010')
-token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxMSIsInJvbGUiOlsiZ3Vlc3QiXSwiX2lkIjoiNWFkMGM0OWU4MWVkNDY3NWE0ZmNkMTI4IiwiaWF0IjoxNTIzNzAyMzg4LCJleHAiOjE1MjQzMDcxODh9.2ulNBVcB5sVfJqRMUP-EUutoJExuoLqr3nqo0SLeiYU'
+let token = localStorage.getItem('harvinStudentToken');
+console.log('token', token);
+if (url == 'http://localhost:3010')
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxMSIsInJvbGUiOlsiZ3Vlc3QiXSwiX2lkIjoiNWFkMGM0OWU4MWVkNDY3NWE0ZmNkMTI4IiwiaWF0IjoxNTIzNzAyMzg4LCJleHAiOjE1MjQzMDcxODh9.2ulNBVcB5sVfJqRMUP-EUutoJExuoLqr3nqo0SLeiYU';
 setAuthToken(token);
 const store = createStore(rootReducer, enhancer);
 let App = null;
 const fakeAuth = {
-  isAuthenticated: true,
+  isAuthenticated: false,
   authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
   },
+
   signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100) // fake async
-  }
-}
-const studentHome=null
+    this.isAuthenticated = false;
+    setTimeout(cb, 100); // fake async
+  },
+};
+const studentHome = null;
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={(props) => (
     fakeAuth.isAuthenticated === true
@@ -50,27 +51,25 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
       : <Redirect to='/login' />
   )} />
 
-)
-if(token){
-console.log('token',token)
-App = (<Provider store={store}>
+);
+if (token) {
+  console.log('token', token);
+  App = (<Provider store={store}>
   <Router history={hist}>
     <Switch>
- 
+
         <Route path="/public" component={Public}/>
         <Route path="/login" component={Login}/>  {indexRoutes.map((prop, key) => {
-        return (
-          <PrivateRoute path={prop.path} component={prop.component} key={key} />
-        );
-      })}
+          return (
+            <PrivateRoute path={prop.path} component={prop.component} key={key} />
+          );
+        })}
 
     </Switch>
   </Router>
 </Provider>);
-}
-else
-App=(<Login/>)
-ReactDOM.render(App
-  ,
-  document.getElementById("root")
+} else
+App = (<Login/>);
+ReactDOM.render(App,
+  document.getElementById('root')
 );
---
+++ client/harvinreact/src/views/Public/Public.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { loginAction } from '../../actions';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect,
+    withRouter
+} from 'react-router-dom';
+import loginStyles from '../../variables/styles/loginStyles';
+import { Grid, Paper, withStyles, Button } from 'material-ui';
+import logo from '../../assets/img/harvinLogo.png';
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+      this.isAuthenticated = true;
+      setTimeout(cb, 100);
+    },
+
+    signout(cb) {
+      this.isAuthenticated = false;
+      setTimeout(cb, 100);
+    },
+  };
+
+const Public = () => <h3>Public</h3>;
+const Protected = () => <h3>Protected</h3>;
+
+class Login extends React.Component {
+  state = {
+      redirectToReferrer: false,
+    };
+  login = (e) => {
+      e.preventDefault();
+      fakeAuth.authenticate(() => {
+          this.setState(() => ({
+              redirectToReferrer: true,
+            }));
+        });
+    };
+
+  render() {
+    const { redirectToReferrer } = this.state;
+    const { classes } = this.props;
+
+    if (redirectToReferrer === true) {
+      return (<Redirect to='/dashboard' />);
+    }
+
+    return (
+        <div className={classes.root} >
+                <Paper >
+                    <Grid container  className={classes.centerContainer}>
+                        <Grid item xs={6} className={classes.centerContainer}>
+                            <img src={logo} alt="harvin logo" style={{ height: '50px' }} />
+                        </Grid>
+                        <Grid item xs={6} >
+                            <Grid container >
+                                <Grid item xs={12} className={classes.centerContainer}>
+                                    <input type="text" />
+                                </Grid>
+                                <Grid item xs={12} className={classes.centerContainer}>
+                                    <input type="password" />
+                                </Grid>
+                                <Grid item xs={12} className={classes.centerContainer}>
+                                    <Button variant="raised" onClick={this.login} className={classes.raisedButton}>Signup</Button>
+                                </Grid>
+
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+    return {};
+  };
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({ loginAction }, dispatch);
+  };
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(loginStyles)(Login)));
