633688a8d1622c9a7e537bc2e57e3a9aa2ec3f09
Gopal sharma <gopal151295@gmail.com>
2018-05-19 11:30:52 +0530

    courses UI fixed - name

--- app/controllers/courses.controller.js
+++ app/controllers/courses.controller.js
@@ -1,11 +1,11 @@
-const errorHandler = require('../errorHandler/index');
-const Course = require('./../models/Course');
-Promise = require('bluebird');
-const mongoose = require('mongoose');
+const errorHandler = require("../errorHandler/index");
+const Course = require("./../models/Course");
+Promise = require("bluebird");
+const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 const findAllCourses = () =>
- new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     Course.find({}, (err, foundCourses) => {
       if (err) reject(err);
       else resolve(foundCourses);
@@ -13,49 +13,64 @@ const findAllCourses = () =>
   });
 
 const findCourseById = courseId =>
-   new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     Course.findById(courseId)
-    .then(foundCourse => resolve(foundCourse))
-    .catch(err => reject(err));
+      .then(foundCourse => resolve(foundCourse))
+      .catch(err => reject(err));
   });
 
-const findOneCourseUsingName = (courseName) =>
-   new Promise((resolve, reject) => {
-    Course.findOne({
-      courseName,
-    }, (err, foundCourse) => {
-      if (err) reject(err);
-      else resolve(foundCourse);
-    });
-  });
-
-const insertInCourse = (course) =>
-   new Promise((resolve, reject) => {
-    Course.updateOne({
-      _id: course._id,
-    }, course, {
-      upsert: true,
-    }, (err, result) => {
-      if (err) reject(err);
-      else {
-        resolve(true);
+const findOneCourseUsingName = courseName =>
+  new Promise((resolve, reject) => {
+    Course.findOne(
+      {
+        courseName
+      },
+      (err, foundCourse) => {
+        if (err) reject(err);
+        else resolve(foundCourse);
       }
-    });
+    );
   });
 
-const deleteOneCourse = (courseName) =>
-   new Promise((resolve, reject) => {
-    Course.remove({ courseName }, (err)=> {
-      if (err)reject(err);
+const insertInCourse = course =>
+  new Promise((resolve, reject) => {
+    Course.create(course)
+      .then(createdCourse => resolve(createdCourse))
+      .catch(err => reject(err));
+  });
+
+const updateCourse = course =>
+  new Promise((resolve, reject) => {
+    Course.updateOne(
+      {
+        _id: course._id
+      },
+      course,
+      {
+        upsert: true
+      },
+      (err, result) => {
+        if (err) reject(err);
+        else {
+          resolve(true);
+        }
+      }
+    );
+  });
+
+const deleteOneCourse = courseName =>
+  new Promise((resolve, reject) => {
+    Course.remove({ courseName }, err => {
+      if (err) reject(err);
       resolve(courseName);
     });
-
   });
 
 module.exports = {
   findAllCourses,
   findCourseById,
   insertInCourse,
+  updateCourse,
   deleteOneCourse,
-  findOneCourseUsingName,
+  findOneCourseUsingName
 };
--- app/routes/courses.js
+++ app/routes/courses.js
@@ -1,42 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const validator = require('validator');
-const errorHandler = require('../errorHandler');
-const coursesCont = require('../controllers/courses.controller');
-const fileController = require('../controllers/file.controller');
-const path = require('path');
-const COURSEIMAGE_SAVE_LOCATION = path.normalize(__dirname + '/../../../HarvinDb/courseImages/');
+const validator = require("validator");
+const errorHandler = require("../errorHandler");
+const coursesCont = require("../controllers/courses.controller");
+const fileController = require("../controllers/file.controller");
+const path = require("path");
+const COURSEIMAGE_SAVE_LOCATION = path.normalize(
+  __dirname + "/../../../HarvinDb/courseImages/"
+);
 
-router.get('/new', (req, res) => {
-  res.render('newCourse');
+router.get("/new", (req, res) => {
+  res.render("newCourse");
 });
 
-router.get('/all', async (req, res, next) => {
+router.get("/all", async (req, res, next) => {
   try {
     const foundCourses = await coursesCont.findAllCourses();
-    res.render('coursesList', { foundCourses });
+    res.render("coursesList", { foundCourses });
   } catch (err) {
-    next(err || 'Internal Server Error');
+    next(err || "Internal Server Error");
   }
 });
 
-router.get('/:courseId/edit', async (req, res, next) => {
+router.get("/:courseId/edit", async (req, res, next) => {
   res.locals.flashUrl = req.header.referer;
 
-  const courseId = req.params.courseId || '';
+  const courseId = req.params.courseId || "";
   if (!courseId || !validator.isMongoId(courseId)) {
-    return errorHandler.errorResponse('INVALID_FIELD', 'course id', next);
+    return errorHandler.errorResponse("INVALID_FIELD", "course id", next);
   }
 
   try {
     const foundCourse = await coursesCont.findCourseById(courseId);
-    res.render('editCourse', { foundCourse });
+    res.render("editCourse", { foundCourse });
   } catch (err) {
-    next(err || 'Internal Server Error');
+    next(err || "Internal Server Error");
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  // console.log("body", req.body);
+
   let file = req.files.courseImage;
 
   // let courseImage
@@ -47,12 +51,12 @@ router.post('/', async (req, res, next) => {
     try {
       await fileController.uploadFileToDirectory(filePath, file);
     } catch (err) {
-      return next(err || 'Internal Server Error');
+      return next(err || "Internal Server Error");
     }
   }
 
   const courseName = req.body.courseName;
-  const courseTimings = req.body.courseTimings;
+  const courseTimings = req.body.classTimings;
   const courseStartingFrom = req.body.courseStartingFrom;
   const courseDescription = req.body.courseDescription;
   const courseFor = req.body.courseFor;
@@ -60,33 +64,48 @@ router.post('/', async (req, res, next) => {
   const courseFrequency = req.body.courseFrequency;
   const _id = req.body.courseId;
   const course = {
-    _id,
     courseName,
     courseTimings,
     courseStartingFrom,
     courseDescription,
     courseFor,
     courseAdmissionThrough,
-    courseFrequency,
+    courseFrequency
   };
 
   if (file) {
     course.courseImage = file.name;
   }
 
-  coursesCont.insertInCourse(course).then(result => {
-    if (result) {
-      req.flash('success', 'Course Inserted Successfully');
-      res.redirect('/admin/courses/all');
-    }
-  }).catch(err => next(err || 'Internal Server Error'));
+  if (_id) {
+    course._id = _id;
+    coursesCont
+      .updateCourse(course)
+      .then(result => {
+        if (result) {
+          req.flash("success", "Course Inserted Successfully");
+          res.redirect("/admin/courses/all");
+        }
+      })
+      .catch(err => next(err || "Internal Server Error"));
+  } else {
+    coursesCont
+      .insertInCourse(course)
+      .then(result => {
+        if (result) {
+          req.flash("success", "Course Inserted Successfully");
+          res.redirect("/admin/courses/all");
+        }
+      })
+      .catch(err => next(err || "Internal Server Error"));
+  }
 });
-router.delete('/delete/:courseName', (req, res, next) => {
+router.delete("/delete/:courseName", (req, res, next) => {
   try {
     const courseName = coursesCont.deleteOneCourse(req.params.courseName);
     res.sendStatus(200);
   } catch (err) {
-    next(err || 'Internal Server Error');
+    next(err || "Internal Server Error");
   }
 });
 module.exports = router;
