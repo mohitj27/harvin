8c4914ef173577cc812b9329c9e6638262da2e29
Gopal sharma <gopal151295@gmail.com>
2018-05-25 13:06:59 +0530

    Separated Questions from add questions

--- client/harvinreact/src/variables/styles/questionsStyle.jsx
+++ client/harvinreact/src/variables/styles/questionsStyle.jsx
@@ -2,33 +2,10 @@
 // // // Login styles
 // #############################
 
-const loginStyles = {
-  root: {
-    display: "flex",
-    height: "-webkit-fill-available",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  rootPaper: {},
-  raisedButton: {
-    backgroundColor: "#13b38b",
-    color: "white",
-    marginBottom: "20px"
-  },
-  centerContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  marginBottom: {
-    marginBottom: "4px"
-  },
-  formControl: {
-    width: "100%"
-  },
-  centerItem: {
-    padding: "10px"
+const qustionStyle = {
+  loading: {
+    minHeight: "60vh"
   }
 };
 
-export default loginStyles;
+export default qustionStyle;
--- client/harvinreact/src/views/Questions/Questions.jsx
+++ client/harvinreact/src/views/Questions/Questions.jsx
@@ -3,31 +3,165 @@ import PropTypes from "prop-types";
 import questionsStyle from "../../variables/styles/questionsStyle";
 import { getAllQuestions } from "../../actions";
 
+import * as actions from "../../actions";
 import classnames from "classnames";
 import { connect } from "react-redux";
 
-import {} from "material-ui-icons";
-
+import {
+  ErrorSnackbar,
+  SuccessSnackbar,
+  LoadingSnackbar
+} from "../../components/GlobalSnackbar/GlobalSnackbar";
+import { ExpandMore } from "material-ui-icons";
+import HtmlToReact from "html-to-react";
+import {
+  Button,
+  Grid,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  Typography,
+  Checkbox,
+  IconButton
+} from "material-ui";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-
 import { RegularCard, ItemGrid } from "../../components";
+
+const HtmlToReactParser = HtmlToReact.Parser;
+
 class Questions extends Component {
+  state = {
+    questions: null
+  };
+
   getCardContent() {
-    return <div />;
+    let { classes } = this.props;
+    let errorSnackbar =
+      this.props.errorMessage !== "" ? (
+        <ErrorSnackbar
+          errorMessage={this.props.errorMessage}
+          onClearToast={this.props.onClearToast}
+        />
+      ) : null;
+
+    let content = null;
+    if (!this.props.isFetchingAllQuestionsInProgress) {
+      if (this.state.questions.length > 0) {
+        content = this.state.questions.map((ques, i) => {
+          let htmlToReactParser = new HtmlToReactParser();
+          let reactElement = htmlToReactParser.parse(ques.question);
+          return (
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+                <Typography style={{ alignSelf: "center", marginRight: "5px" }}>
+                  Q.{i + 1}
+                </Typography>{" "}
+                {reactElement}
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Grid style={{ display: "flex", flexWrap: "wrap" }}>
+                  {this.getPrevQuesOptions(ques.options)}
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          );
+        });
+      } else {
+        content = (
+          <Grid
+            style={{ display: "flex" }}
+            justify="center"
+            className={classes.loading}
+          >
+            <h4>You haven't added any question yet !!!</h4>
+          </Grid>
+        );
+      }
+    } else {
+      content = (
+        <Grid
+          style={{ display: "flex" }}
+          justify="center"
+          className={classes.loading}
+        >
+          <CircularProgress />
+        </Grid>
+      );
+    }
+    return (
+      <Fragment>
+        {errorSnackbar}
+        {content}
+      </Fragment>
+    );
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.onQuestionsFetch();
+  }
+
+  static getDerivedStateFromProps = (nextProps, prevState) => {
+    console.log("next", nextProps);
+
+    if (
+      !nextProps.isFetchingAllQuestionsInProgress &&
+      nextProps.questions.length >= 0
+    ) {
+      return {
+        questions: nextProps.questions
+      };
+    }
+    return null;
+  };
+
+  getPrevQuesOptions = opts => {
+    const options = opts || [];
+    let htmlToReactParser = new HtmlToReactParser();
+
+    return options.map((opt, i) => {
+      let reactElement = htmlToReactParser.parse(opt.text);
+
+      return (
+        <Fragment>
+          <Checkbox
+            checked={opt.isAns}
+            value={`${opt.text}`}
+            disabled={true}
+            color="primary"
+          />
+
+          {reactElement}
+        </Fragment>
+      );
+    });
+  };
 
   render() {
+    let successSnackbar =
+      this.props.successMessage !== "" ? (
+        <SuccessSnackbar
+          successMessage={this.props.successMessage}
+          onClearToast={this.props.onClearToast}
+        />
+      ) : null;
+    let errorSnackbar =
+      this.props.errorMessage !== "" ? (
+        <ErrorSnackbar
+          errorMessage={this.props.errorMessage}
+          onClearToast={this.props.onClearToast}
+        />
+      ) : null;
+    let loadingSnackbar =
+      this.props.notifyLoading !== "" ? <LoadingSnackbar /> : null;
     return (
       <Fragment>
         <Grid container="container">
           <ItemGrid xs={12}>
             <RegularCard
-              cardTitle="Add Questions"
+              cardTitle="Questions"
               cardSubtitle="List of all the questions added by you"
-              headerColor="blueGrey"
+              headerColor="blue"
               content={this.getCardContent()}
             />
           </ItemGrid>
@@ -37,10 +171,22 @@ class Questions extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch) {}
+function mapDispatchToProps(dispatch) {
+  return {
+    onQuestionsFetch: () => dispatch(actions.getAllQuestions())
+  };
+}
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    successMessage: state.notify.success,
+    errorMessage: state.notify.error,
+    notifyLoading: state.notify.loading,
+    notifyClear: state.notify.clear,
+    questions: state.questions.allQuestions,
+    isFetchingAllQuestionsInProgress:
+      state.questions.isFetchingAllQuestionsInProgress
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(
