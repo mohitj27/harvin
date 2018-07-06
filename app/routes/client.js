b21ea8273e0b385b6b54c14983ce211469fef4b6
Ankush Grover <ankushtiwari479@gmail.com>
2018-07-06 15:54:41 +0530

    some changes related to pixel and quiz build route --ankush

--- app/routes/client.js
+++ app/routes/client.js
@@ -4,11 +4,17 @@ const middleware = require('../middleware')
 const path = require("path");
 
 router.get("/", (req, res) => {
-    // res.render("login");
-    // console.log("here");
-    res.sendFile(path.join(__dirname, "../../client/harvinreact/build/app.html"));
-  
-  });
-  
+  // res.render("login");
+  // console.log("here");
+  res.sendFile(path.join(__dirname, "../../client/harvinreact/build/app.html"));
+
+});
+router.get("*", (req, res) => {
+  // res.render("login");
+  // console.log("here");
+  res.sendFile(path.join(__dirname, "../../client/harvinreact/build/app.html"));
+
+});
+
 
 module.exports = router
