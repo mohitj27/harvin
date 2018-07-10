93e11a522abf3a5ca196e5a512ac7c6e16c76eed
rishabh570 <www.rishabhrawat570@gmail.com>
2018-07-10 13:05:03 +0530

    rishabh: shift static files to gulp architecture. (phase 1).

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const cleanCss = require('gulp-clean-css');
+
+
+// Optimize Images
+gulp.task('imageMin', function() {
+	gulp.src([
+        'app/public/assets/img/**/*.+(jpg|png)',
+        'app/public/assets/img/**/**/*.+(jpg|png)',
+        'app/public/assets/img/*.+(png|jpg)',
+    ])
+	.pipe(imagemin())
+	.pipe(gulp.dest('app/dist/images'))
+});
+
+// Minify JS
+// gulp.task('minifyJs', function(){
+//     return gulp.src([
+//         'app/public/assets/js/main.js'
+//         ])
+//         .pipe(uglify())
+//         .pipe(gulp.dest('app/dist/js'))
+// });
+
+// Clean CSS
+
+gulp.task('cleanCss', function () {	
+    gulp.src([
+        'app/public/assets/stylesheets/*.css',
+        'app/public/css/*.css'
+    ])
+    .pipe(cleanCss())
+	.pipe(gulp.dest('app/dist/css'))
+});
+
+gulp.task('default', ['imageMin', 'cleanCss']);
